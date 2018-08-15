1113bf7ff4c3966dcb135e2c5ca51a5a6f5c9592
daniux <victorxdy@gmail.com>
2018-08-15 09:06:49 -0700

    Update RestaurantGridFragment.java

--- RestaurantGridFragment.java
+++ RestaurantGridFragment.java
@@ -29,14 +29,6 @@ public class  RestaurantGridFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        /*
-        // Inflate the layout for this fragment
-        // return inflater.inflate(R.layout.fragment_restaurant_grid, container, false);
-        View view = inflater.inflate(R.layout.fragment_restaurant_grid, container, false);
-        mGridView = (GridView) view.findViewById(R.id.restaurant_grid_view);
-        mGridView.setAdapter(new RestaurantAdapter(getActivity()));
-        return view;
-        */
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_restaurant_grid, container, false);
         mGridView = (GridView) view.findViewById(R.id.restaurant_grid_view);
